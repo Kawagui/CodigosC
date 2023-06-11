ca6df6f997fa357fc400cc354ce7e1a43870cd0e
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-06-11 19:08:59 -0300

    conversao.c
    Menu inicial

--- conversao.c
+++ conversao.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+//int dec (int x, ){
+//}
+//int hex (int x){
+//}
+int main(){
+  int m=0;
+	printf("Escolha o que quer converter:\n 1-Tempo\n 2-Temperatura\n 3-Volume\n 4-Velocidade\n 5-Densidade\n 6-Sair");
+  scanf("%d", &m);
+	return 0;
+}
