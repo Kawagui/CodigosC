8ce1b5efcda11eb391b891c2a7119be2f8690e63
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-06-15 15:04:40 -0300

    Opoligono.c

--- poligono.c
+++ poligono.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main (){
-  int N=0, d;
-  float ai, ae, S;
+  float  N=0, d, ai, ae, S;
   while (N<=2){
     printf("Insira um número de lados para um polígono regular: ");
     scanf("%d", &N);
   }
-  d=N(N-3)/2;
+  d=N*(N-3)/2;
   S=(N-2)*180;
   ai=S/N;
   ae=180-ai;
-  printf("\nO polígono de %d lados possui:\n%d diagonais\n%.2f=ângulo interno\n%,2f=ângulo externo\n%.2f=soma dos ângulos internos", N,d,ai,ae,S);
+  printf("\nO polígono de %.0f lados possui:\n%.0f diagonais\n%.2f=ângulo interno\n%.2f=ângulo externo\n%.0f=soma dos ângulos internos", N,d,ai,ae,S);
 return 0;
 }
