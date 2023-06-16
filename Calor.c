edc48ca6838a86972af21ca9b4ba42b7e2f8fe6c
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-06-16 17:06:30 -0300

    Calor.c

---
+++ Calor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void massa{
+printf("Digite o valor da massa em gramas: ");
+  scanf("%f",&v[1]);
+}
+void calor
+void temperatura
+void coeficiente
+int main (){
+  int i;
+  float a[4]; //a[0] calor, a[1] massa, a[2] calor sensivel, a[3] temperatura
+  printf("\n");
+  scanf("%d",&i);
+  switch(i){
+    case 0:
+      
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break
+}
+  return 0;
+}
