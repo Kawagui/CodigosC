505f2ac74a5ea7c39e9f32145ee2d52cba7e124e
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-06-16 18:04:13 -0300

    Calor.c

--- Calor.c
+++ Calor.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
-void massa{
-printf("Digite o valor da massa em gramas: ");
-  scanf("%f",&v[1]);
+float a[4]; //a[0] calor, a[1] massa, a[2] calor sensivel, a[3] temperatura
+void massa(){
+    printf("\nDigite o valor da massa em gramas: ");
+    scanf("%f",&a[1]);
 }
-void calor
-void temperatura
-void coeficiente
+void energia(){
+    printf("\nDigite o valor da energia em calorias: ");
+    scanf("%f",&a[0]);
+}
+void temperatura(){
+    printf("\nDigite o valor da variação de temperatura em Celsius: ");
+    scanf("%f",&a[3]);
+}
+void coeficiente(){
+    printf("\nDigite o valor do coeficiente de calor sensível em caloria / grama * Celsius: ");
+    scanf("%f",&a[2]);
+}
 int main (){
-  int i;
-  float a[4]; //a[0] calor, a[1] massa, a[2] calor sensivel, a[3] temperatura
-  printf("\n");
-  scanf("%d",&i);
-  switch(i){
-    case 0:
-      
-      break;
-    case 1:
-      break;
-    case 2:
-      break;
-    case 3:
-      break
+    int i;
+    printf("\n");
+    scanf("%d",&i);
+    switch(i){
+      case 0:
+        massa();
+        temperatura();
+        coeficiente();
+        a[i]=a[1]*a[2]*a[3];
+        printf("%.0f calorias", a[i]);
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
 }
   return 0;
 }
