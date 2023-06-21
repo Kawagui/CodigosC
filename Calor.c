c3a9cb205541b7b399547dffcea89730f7e06bea
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-06-21 02:54:32 -0300

    Calor.c

--- Calor.c
+++ Calor.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 float a[4]; //a[0] calor, a[1] massa, a[2] calor sensivel, a[3] temperatura
-char m[4][3]={{cal},{g},{/},{°C}};
+char m[4][5]={{"cal"},{"g"},{"/"},{"°C"}};
 void massa(){
     printf("\nDigite o valor da massa em gramas: ");
     scanf("%f",&a[1]);
@@ -15,12 +15,12 @@ void temperatura(){
     scanf("%f",&a[3]);
 }
 void coeficiente(){
-    printf("\nDigite o valor do coeficiente de calor sensível em caloria / grama * Celsius: ");
+    printf("\nDigite o valor do calor específico da substância em calorias / gramas * graus Celsius: ");
     scanf("%f",&a[2]);
 }
 int main (){
     int i;
-    printf("\nQual valor você quer descobrir?\n 0-Energia\n 1-Massa\n 2-Coeficiente de calor sensível\n 3-Variação de temperatura");
+    printf("Qual valor você quer descobrir?\n 0-Energia\n 1-Massa\n 2-Coeficiente de calor sensível\n 3-Variação de temperatura\n");
     scanf("%d",&i);
     switch(i){
       case 0:
@@ -52,5 +52,6 @@ int main (){
         printf("%.0f %s", a[i], m[i]);
         break;
 }
+  printf("\n");
   return 0;
 }
