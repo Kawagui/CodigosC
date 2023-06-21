0d264096b542fbab648c6ad03cf2e817243741cb
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-06-21 03:53:07 -0300

    AnaCombi.c
    Pronto

--- AnaCombi.c
+++ AnaCombi.c
@@ -1,63 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-int calFat(int n) {
+float N= -1, P= -1, n= -1, p= -1, r, R,y, x;
+int M= -1;
+int calFat(float n) {
     if (n == 0) {
         return 1;
     } else {
-        return n * calcularFatorial(n - 1);
+    	return (n * calFat(n - 1));
     }
 }
-void permu (float x){
-	return (calFat(x))
+int permu (float x){
+	return (calFat(x));
 }
-void arranjo (float x,p){
-	return (calFat(x)/calFat(x-p))
+int arranjo (float x, float p){
+	return (calFat(x)/calFat(x-p));
 }
-void combi (float x,p){
-    return (calFat(x)/calFat(p)(calFat(x-p)))
+int combi (float x,float p){
+    return (calFat(x)/(calFat(p)*calFat(x-p)));
 }
-void prob
-
+void LerN(){
+	while (N<0){
+		printf("Digite o valor de N: ");
+		scanf("%f",&N);
+    }
+}
+void LerP(){
+	while (P<0){	
+		printf("Digite o valor de P: ");
+		scanf("%f",&P);
+	}
+}
+void WriteR(){
+	printf("%.0f possibilidades\n\n", R);
+	N= -1, M= -1, P= -1;
+}
+void Lern(){
+	while (n<0){
+		printf("\nDigite o valor de n para o campo de possibilidades: ");
+		scanf("%f",&n);
+    }
+}
+void Lerp(){
+	while (p<0){
+		printf("\nDigite o valor de p para o campo de possibilidades: ");
+		scanf("%f",&p);
+    }
+}
+void Writer(){
+	printf("A probabilidade é de %.0f/%.0f\n", r, R);
+	N= -1, M= -1, P= -1, n= -1, p= -1;
+}
 int main() {
-    int M = -1; 
-	float N= -1, R, r, P= -1, n= -1, p= -1;
     while (M!=5){
         while (M<0 || M>5){  
-            printf("Selecione uma expressão:\n1-Permutação simples\n2-Arranjo simples\n3-Combinação simples\n4-Probabilidade\n5-Sair\n");
+            printf("Selecione uma expressão:\n 1-Permutação simples\n 2-Arranjo simples\n 3-Combinação simples\n 4-Probabilidade\n 5-Sair\n");
             scanf("%d", &M);
         }
 	    switch(M){
 		    case 1:
-			    while (N<0){
-			        printf("Digite o valor de N ");
-			        scanf("%f",&N);
-                }
+			    LerN();
 			    R=permu(N);
-			    printf("\n%f possibilidades", R)
-    		break;
+			    WriteR();
+    			break;
 		    case 2:
-                while (N<0){
-			        printf("Digite o valor de N ");
-			        scanf("%f",&N);
-			    }
-                while (P<0){
-			        printf("Digite o valor de P ");
-			        scanf("%f",&P);
-			    }
+                LerN();
+                LerP();
                 R=arranjo(N,P);
-    			printf("\n%f possibilidades", R)
+    			WriteR();
             break;
             case 3:
-			    while (N<0){
-			        printf("Digite o valor de N ");
-			        scanf("%f",&N);
-			    }
-                while (P<0){
-			        printf("Digite o valor de P ");
-			        scanf("%f",&P);
-			    }    
+			   	LerN();
+                LerP();   
                 R=combi(N,P);
-			    printf("\n%f possibilidades", R)
+			    WriteR();
             break;
             case 4: //verificar qual caso de probabilidade
                 M=-1;
@@ -67,60 +82,31 @@ int main() {
                 }
                 switch(M){
 		            case 1:
-			            while (N<0){
-			                printf("Digite o valor de N para todas as possibilidades");
-			                scanf("%f",&N);
-                        }
+			            LerN();
 			            R=permu(N);
-                        while (n<0){
-			                printf("Digite o valor de n para o campo de possibilidades");
-			                scanf("%f",&n);
-                        }
+                        Lern();
 			            r=permu(n);
     		        break;
 		            case 2:
-                        while (N<0){
-		    	            printf("Digite o valor de N ");
-			                scanf("%f",&N);
-			            }
-                        while (P<0){
-			                printf("Digite o valor de P ");
-			                scanf("%f",&P);
-			            }
+                        LerN();
+                		LerP();
                         R=arranjo(N,P);
-                        while (n<0){
-			                printf("Digite o valor de n para o campo de possibilidades");
-			                scanf("%f",&n);
-                        }
-                        while (p<0){
-			                printf("Digite o valor de p para o campo de possibilidades");
-			                scanf("%f",&p);
-                        }
+                        Lern();
+                		Lerp();
 			            r=arranjo(n,p);
                     break;
                     case 3:
-			            while (N<0){
-			                printf("Digite o valor de N ");
-			                scanf("%f",&N);
-			            }
-                        while (P<0){
-			                printf("Digite o valor de P ");
-			                scanf("%f",&P);
-			            }    
+			            LerN();
+                		LerP();
                         R=combi(N,P);
-                        while (n<0){
-			                printf("Digite o valor de n para o campo de possibilidades");
-			                scanf("%f",&n);
-                        }
-                        while (p<0){
-			                printf("Digite o valor de p para o campo de possibilidades");
-			                scanf("%f",&p);
-                        }
+                        Lern();
+                		Lerp();
                         r=combi(n,p);
                     break;
-                printf("A probabilidade é de %f/%f", r, R);
+				}
+                Writer();
             break;
-	        }
+	    }
     }
     return 0;
 }
