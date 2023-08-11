a104ff636c71fc5865c99536abad10d6602dc24d
Kawagui <135202943+Kawagui@users.noreply.github.com>
2023-08-10 22:23:44 -0300

    Update AnaCombi.c

--- AnaCombi.c
+++ AnaCombi.c
@@ -2,81 +2,91 @@
 #include <stdlib.h>
 float N= -1, P= -1, n= -1, p= -1, r, R,y, x;
 int M= -1;
-int calFat(float n) {
+int calFat(float n) {	//Fatorial recursivo
     if (n == 1) {
         return 1;
     } else {
     	return (n * calFat(n - 1));
     }
 }
-int arrarepete (float x, float p){
-    
-int permu (float x){
+void arrarepete (float x, float p){ 	//Arranjo com repetição
+	R=1;
+	for (p; p!=0; p--){
+		R=x*R;
+	}
+}
+int combirepete (float x, float p){	//Combinação com repetição
+	return ((calFat(x+p-1))/(calFat(p)*calFat(x-1)));
+}
+int permucircu (float x){		//Permutação com repetição
+	return (calFat(x)/x);
+}    
+int permu (float x){	//Permutação simples
 	return (calFat(x));
 }
-int arranjo (float x, float p){
+int arranjo (float x, float p){ 	//Arranjo simples
 	return (calFat(x)/calFat(x-p));
 }
-int combi (float x,float p){
+int combi (float x,float p){	//Combinação simples
     return (calFat(x)/(calFat(p)*calFat(x-p)));
 }
-void LerN(){
+void LerN(){	//Ler variável N
 	while (N<0){
 		printf("Digite o valor de N: ");
 		scanf("%f",&N);
     }
 }
-void LerP(){
+void LerP(){ 	//Ler variável P
 	while (P<0){	
 		printf("Digite o valor de P: ");
 		scanf("%f",&P);
 	}
 }
-void WriteR(){
+void WriteR(){		//Escrever variável R
 	printf("%.0f possibilidades\n\n", R);
 	N= -1, M= -1, P= -1;
 }
-void Lern(){
+void Lern(){	//Ler variável n
 	while (n<0){
 		printf("\nDigite o valor de n para o campo de possibilidades: ");
 		scanf("%f",&n);
     }
 }
-void Lerp(){
+void Lerp(){	//Ler variável p
 	while (p<0){
 		printf("\nDigite o valor de p para o campo de possibilidades: ");
 		scanf("%f",&p);
     }
 }
-void Writer(){
-	printf("A probabilidade é de %.0f/%.0f\n", r, R);
+void Writer(){		//Escrever variável r
+	printf("A probabilidade é de %.0f/%.0f\n\n", r, R);
 	N= -1, M= -1, P= -1, n= -1, p= -1;
 }
 int main() {
-    while (M!=5){
-        while (M<0 || M>5){  
-            printf("Selecione uma expressão:\n 1-Permutação simples\n 2-Arranjo simples\n 3-Combinação simples\n 4-Probabilidade\n 5-Sair\n");
+    while (M!=8){
+        while (M<0 || M>8){  
+            printf("Selecione uma expressão:\n 1-Permutação simples\n 2-Arranjo simples\n 3-Combinação simples\n 4-Probabilidade\n 5-Permutação circular\n 6-Arranjo com repetição\n 7-Combinação com repetição\n 8-Sair\n\n");
             scanf("%d", &M);
         }
 	    switch(M){
-		    case 1:  //permutação simples
+		    case 1:  	//permutação simples
 			    LerN();
 			    R=permu(N);
 			    WriteR();
     			break;
-		    case 2: //arranjo simples
+		    case 2: 	//arranjo simples
                 LerN();
                 LerP();
                 R=arranjo(N,P);
     			WriteR();
             break;
-            case 3: //combinação simples
+            case 3: 	//combinação simples
 			   	LerN();
                 LerP();   
                 R=combi(N,P);
 			    WriteR();
             break;
-            case 4: //verificar qual caso de probabilidade
+            case 4: 	//verificar qual caso de probabilidade
                 M=-1;
                 while (M<1 || M>4){
                     printf("Qual o tipo de probabilidade?\n1-Permutação simples\n2-Arranjo simples\n3-Combinação simples\n4-Sair\n");
@@ -108,14 +118,25 @@ int main() {
 				}
                 Writer();
             break;
-            case 5: //permutação com repetição
-			   	//(n!/n1!*n2!*n3!*...nk!) k = número de repetições diferentes
+            case 5: 	//permutação circular
+			   	LerN();
+				R=permucircu(N);
+			   	WriteR();
             break;
-            case 6: //arranjo com repetição
-			   	//(n^k)
+            case 6: 	//arranjo com repetição
+            	LerN();
+            	LerP();
+			   	arrarepete(N,P);	
+			   	WriteR();
             break;
-            case 7: //combinação com repetição
-			   	//(n+k-1)!/(k!*(n-1)!)
+            case 7: 	//combinação com repetição
+            	LerN();
+            	LerP();
+			   	R=combirepete(N,P);
+			   	WriteR();
+            break;
+            case 8:
+            	printf("Encerrando\n");
             break;
 	    }
     }
