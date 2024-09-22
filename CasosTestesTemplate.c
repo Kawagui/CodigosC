705261c043a4770d2068262efe765c82c830e249
Kawagui <135202943+Kawagui@users.noreply.github.com>
2024-09-22 16:59:43 -0300

    Create CasosTestesTemplate.c

---
+++ CasosTestesTemplate.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+//Template para caso de testes
+int main(){
+    int Testes;
+    scanf("%d", &Testes); //Ler número de testes
+    while((Testes--)>0){ //Colocar código para testar n vezes
+
+    }
+    return 0;
+}
