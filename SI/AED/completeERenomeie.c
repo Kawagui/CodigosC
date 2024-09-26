3a59cdb1dd8fc9f45f4d37e96b5ade008cdba611
Kawagui <135202943+Kawagui@users.noreply.github.com>
2024-09-26 19:53:18 -0300

    Update completeERenomeie.c

--- SI/AED/completeERenomeie.c
+++ SI/AED/completeERenomeie.c
@@ -8,28 +8,33 @@
 /**                                                                 **/
 /*********************************************************************/
 
-
 /*
-A Maratona de Programacao que voce gerenciara tem um numero fixo de problemas definido na constante PROBLEMAS. Os problemas da maratona serao numerados de 0 (zero) ate PROBLEMAS-1. Havera uma lista ligada ordenada pela classificacao dos times, como discutido ao longo do enunciado.
+A Maratona de Programacao que voce gerenciara tem um numero fixo de problemas definido na constante PROBLEMAS.
+Os problemas da maratona serao numerados de 0 (zero) ate PROBLEMAS-1.
+Havera uma lista ligada ordenada pela classificacao dos times, como discutido ao longo do enunciado.
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-#define false 0
-#define true 1
+#define false 0 //false possui valor 0
+#define true 1 //true possui valor 1
 
-#define PROBLEMAS 10
+#define PROBLEMAS 10 //Número de problemas 0 a 9
 
-typedef int bool;
-
+typedef int bool; //Definir tipo bool como int
 
 /*
-A estrutura  TIME foi projetada para armazenar as informacoes de cada time participante da Maratona de Programacao. Ela e composta por quatro campos:
+A estrutura TIME foi projetada para armazenar as informacoes de cada time participante da Maratona de Programacao.
+Ela e composta por quatro campos:
    id – identificador numerico do time, comecando por 1 (um);
-   resolvidos – arranjo booleano com tamanho igual ao numero de problemas da maratona, que contera o valor true}, caso o tinha tenha resolvido (corretamente) o problema e false}, caso contrario;
-   tentativas – arranjo de inteiros com tamanho igual ao numero de problemas da maratona, que contera o numero de tentativas (submissoes) do respectivo problema que o time fez ate conseguir resolver o problema; e
-   minutos – arranjo de inteiros com tamanho igual ao numero de problemas da maratona, que contera o numero de minutos passados desde o inicio da maratona ate o momento em que o time submeteu a solucao correta do respectivo problema. Para os problemas em que o time nao submeteu uma solucao correta, o valor 0 (zero) deve ser armazenado na respectiva posicao do arranjo.
+   resolvidos – arranjo booleano com tamanho igual ao numero de problemas da maratona,
+   	que contera o valor true}, caso o Time tenha resolvido (corretamente) o problema e false}, caso contrario;
+   tentativas – arranjo de inteiros com tamanho igual ao numero de problemas da maratona,
+   	que contera o numero de tentativas (submissoes) do respectivo problema que o time fez ate conseguir resolver o problema; e
+   minutos – arranjo de inteiros com tamanho igual ao numero de problemas da maratona,
+   	que contera o numero de minutos passados desde o inicio da maratona ate o momento em que o time submeteu a solucao correta do respectivo problema.
+    	Para os problemas em que o time nao submeteu uma solucao correta, o valor 0 (zero) deve ser armazenado na respectiva posicao do arranjo.
 */
 typedef struct {
 	int id;
@@ -39,8 +44,9 @@ typedef struct {
 } TIME;
 
 /*
-A estrutura  ELEMENTO foi projetada para armazenar cada elemento da lista duplamente ligada, ordenada, com no cabeca e nao circular que voce gerenciara. Esta estrutura e composta por tres campos:
-   time – ponteiro (ou referencia) para o time participante da maratona;
+A estrutura ELEMENTO foi projetada para armazenar cada elemento da lista duplamente ligada, ordenada, com no cabeca e nao circular
+	que voce gerenciara. Esta estrutura e composta por tres campos:
+   time (chave) – ponteiro (ou referencia) para o time participante da maratona;
    ant – ponteiro (ou referencia) para o elemento anterior da lista ligada; e
    prox – ponteiro (ou referencia) para o elemento seguinte da lista ligada.
 */
@@ -49,7 +55,6 @@ typedef struct aux{
 	struct aux* ant;
 	struct aux* prox;
 } ELEMENTO, *PONT;
-
 
 /*
 A estrutura  MARATONA foi projetada para representar a Maratona de Programacao. Esta estrutura e composta por dois campos:
@@ -61,7 +66,6 @@ typedef struct {
 	int numTimes;
 } MARATONA;
 
-
 /*
 Funcao usada em testes da correcao automatica
    NAO APAGAR OU MODIFICAR 
@@ -70,9 +74,16 @@ void print123(){
 
 }
 
-
 /*
-Funcao que recebe o endereco de um TIME e, se esse endereco for igual a NULL, retorna -1. Caso contrario, calcula e retorna a penalidade total (em minutos) recebida pelo time. A penalidade total de um time e dada pela soma dos minutos que o time levou para resolver cada problema (estes valores sao armazenados no arranjo minutos), considerando apenas os problemas efetivamente resolvidos, mais 20 minutos de penalizacao para cada submissao incorreta, considerando, novamente, apenas os problemas que o time conseguiu resolver. Por exemplo, se o time conseguiu resolver o problema numero 3 apenas na quinta tentativa e a tentativa correta ocorreu no minuto 110, havera uma penalizacao de 80 minutos referentes as tentativas incorretas, mais 110 minutos referentes ao tempo que o time levou para resolver esse problema. Assim, havera uma penalidade de 190 minutos referente ao problema de numero 3 (a penalidade total do time corresponde a soma das penalidades do time para todos os problemas da maratona). Destaca-se que problemas nao resolvidos corretamente tem penalidade igual a zero.
+Funcao que recebe o endereco de um TIME e, se esse endereco for igual a NULL, retorna -1.
+Caso contrario, calcula e retorna a penalidade total (em minutos) recebida pelo time.
+A penalidade total de um time e dada pela soma dos minutos que o time levou para resolver cada problema
+(estes valores sao armazenados no arranjo minutos), considerando apenas os problemas efetivamente resolvidos,
+mais 20 minutos de penalizacao para cada submissao incorreta, considerando, novamente, apenas os problemas que o time conseguiu resolver.
+Por exemplo, se o time conseguiu resolver o problema numero 3 apenas na quinta tentativa e a tentativa correta ocorreu no minuto 110,
+havera uma penalizacao de 80 minutos referentes as tentativas incorretas, mais 110 minutos referentes ao tempo que o time levou para resolver esse problema.
+Assim, havera uma penalidade de 190 minutos referente ao problema de numero 3 (a penalidade total do time corresponde a soma das penalidades do time para todos os problemas da maratona).
+Destaca-se que problemas nao resolvidos corretamente tem penalidade igual a zero.
 */
 int calcularPenalidade(TIME* time){
 
@@ -81,9 +92,10 @@ int calcularPenalidade(TIME* time){
 	return -1;
 }
 
-
 /*
-Funcao que recebe o endereco de um TIME e, se esse endereco for igual a NULL, retorna -1. Caso contrario, calcula e retorna o numero total de problemas resolvidos pelo respectivo time (o arranjo resolvidos armazena, para cada problema, se ele foi ou nao resolvido).
+Funcao que recebe o endereco de um TIME e, se esse endereco for igual a NULL, retorna -1.
+Caso contrario, calcula e retorna o numero total de problemas resolvidos pelo respectivo time
+(o arranjo resolvidos armazena, para cada problema, se ele foi ou nao resolvido).
 */
 int calcularProblemasResolvidosDoTime(TIME* time){
 
@@ -92,9 +104,10 @@ int calcularProblemasResolvidosDoTime(TIME* time){
 	return -1;
 }
 
-
 /*
-Funcao que recebe o endereco de uma MARATONA e, se esse endereco for igual a NULL, retorna -1. Caso contrario, calcula e retorna o numero total de problemas que nenhum time ainda conseguiu resolver. Isto e, se a maratona tem 10 problemas e ha tres problemas que nenhum time resolveu, devera retornar 3.
+Funcao que recebe o endereco de uma MARATONA e, se esse endereco for igual a NULL, retorna -1.
+Caso contrario, calcula e retorna o numero total de problemas que nenhum time ainda conseguiu resolver.
+Isto e, se a maratona tem 10 problemas e ha tres problemas que nenhum time resolveu, devera retornar 3.
 */
 int calcularProblemasNaoResolvidos(MARATONA* mar){
 
@@ -103,15 +116,26 @@ int calcularProblemasNaoResolvidos(MARATONA* mar){
 	return -1;
 }
 
-
 /*
-Funcao que recebe cinco parametros: mar - endereco de uma MARATONA, id – identificador de um time, problema – identificador de um problema, tempo – tempo, em minutos, da submissao, acerto – parametro booleano que indica se o problema foi resolvido corretamente ou nao. Esta funcao devera retornar false caso algum parametro seja invalido, isto e MAR igual a NULL, ou id fora do intervalo que vai de 1 ate o numero de times da respectiva maratona, ou problema fora do intervalo valido dos problemas (que vai de 0 a PROBLEMAS – 1). A funcao tambem devera retornar false caso o respectivo time ja tenha resolvido corretamente o problema cujo numero e dado pelo parametro problema. Isto e, submissoes de problemas ja resolvidos pelo time devem ser ignoradas. Caso contrario, a funcao deve atualizar as seguintes informacoes do time:
+Funcao que recebe cinco parametros: mar - endereco de uma MARATONA, id – identificador de um time,
+	problema – identificador de um problema, tempo – tempo, em minutos, da submissao,
+ 	acerto – parametro booleano que indica se o problema foi resolvido corretamente ou nao.
+  	Esta funcao devera retornar false caso algum parametro seja invalido, isto e MAR igual a NULL, ou id fora do intervalo
+   	que vai de 1 ate o numero de times da respectiva maratona, ou problema fora do intervalo valido dos problemas (que vai de 0 a PROBLEMAS – 1).
+     	A funcao tambem devera retornar false caso o respectivo time ja tenha resolvido corretamente o problema
+      	cujo numero e dado pelo parametro problema. Isto e, submissoes de problemas ja resolvidos pelo time devem ser ignoradas.
+       	Caso contrario, a funcao deve atualizar as seguintes informacoes do time:
 (i) aumentar em uma unidade o numero de tentativas de resolucao pelo time para o respectivo problema; 
 (ii) caso o time tenha acertado a solucao (parametro acerto = true), (ii.a) atualizar para verdadeiro o respectivo valor do arranjo resolvidos};
 (ii.b) atualizar o valor do arranjo minutos}, atualizando o valor correspondente ao problema atual com o tempo passado como parametro;
 (ii.c) reposicionar, se necessario, o ELEMENTO correspondente ao time atual na lista duplamente ligada, ordenada, com no cabeca e nao circular.
- A lista e ordenada pela classificacao do time na maratona, a qual segue estas regras: a lista e ordenada de forma decrescente do time que resolveu mais problemas para o time que resolveu menos problemas e tendo como criterio de desempate as penalidades recebidas pelos times (dados dois times que resolveram a mesma quantidade de problemas, ficara na frente da lista [mais para o inicio da lista] aquele com menor penalidade).
-Se dois times empatarem nestes dois criterios, deve ficar a frente da lista aquele que ja estava a frente antes da atualizacao (isto e, se o time A ja tinha resolvido dois problemas e tinha penalidade 200 e a funcao tratarSubmissao foi chamada para o time B que, agora, resolveu seu segundo problema e ficou com penalidade 200, o time A deve permanecer a frente na classificacao em relacao ao time B [como ja estava antes desta submissao]). Durante a atualizacao da posicao dos elementos da lista ligada preste bastante atencao para atualizar todos os ponteiros necessarios e nao esqueca que a lista e duplamente ligada e possui no cabeca.
+A lista e ordenada pela classificacao do time na maratona, a qual segue estas regras:
+	a lista e ordenada de forma decrescente do time que resolveu mais problemas para o time que resolveu menos problemas
+	e tendo como criterio de desempate as penalidades recebidas pelos times
+ 	(dados dois times que resolveram a mesma quantidade de problemas, ficara na frente da lista [mais para o inicio da lista] aquele com menor penalidade).
+Se dois times empatarem nestes dois criterios, deve ficar a frente da lista aquele que ja estava a frente antes da atualizacao
+	(isto e, se o time A ja tinha resolvido dois problemas e tinha penalidade 200 e a funcao tratarSubmissao foi chamada para o time B que, agora, resolveu seu segundo problema e ficou com penalidade 200, o time A deve permanecer a frente na classificacao em relacao ao time B [como ja estava antes desta submissao]).
+ 	Durante a atualizacao da posicao dos elementos da lista ligada preste bastante atencao para atualizar todos os ponteiros necessarios e nao esqueca que a lista e duplamente ligada e possui no cabeca.
 */
 bool tratarSubmissao(MARATONA* mar, int id, int problema, int tempo, bool acerto){
 
@@ -120,12 +144,13 @@ bool tratarSubmissao(MARATONA* mar, int id, int problema, int tempo, bool acerto
 	return false;
 }
 
-
-
-
-
 /*
-Funcao que recebe como parametro o endereco de uma MARATONA e o numero de times que participarao da competicao e realiza a inicializacao da maratona. Isto e, cria a lista duplamente ligada com todos os times e o no cabeca, preenche o valor de todos os campos necessarios (valores dos times, elementos e da maratona propriamente dita) e retorna true. Porem, se os parametros recebidos forem invalidos, retorna false}, sem realizar a inicializacao. Destaca-se que durante a inicializacao, a lista ordenada dos times e criada e, neste momento, o time com id igual a 1 esta em primeiro lugar, com id igual a 2 em segundo e assim por diante (se a competicao acabar sem nenhuma resolucao dos problemas, esta sera a classificacao dos times).
+Funcao que recebe como parametro o endereco de uma MARATONA e o numero de times
+que participarao da competicao e realiza a inicializacao da maratona.
+Isto e, cria a lista duplamente ligada com todos os times e o no cabeca, preenche o valor de todos os campos necessarios
+(valores dos times, elementos e da maratona propriamente dita) e retorna true.
+Porem, se os parametros recebidos forem invalidos, retorna false}, sem realizar a inicializacao.
+Destaca-se que durante a inicializacao, a lista ordenada dos times e criada e, neste momento, o time com id igual a 1 esta em primeiro lugar, com id igual a 2 em segundo e assim por diante (se a competicao acabar sem nenhuma resolucao dos problemas, esta sera a classificacao dos times).
 */
 bool inicializarMaratona(MARATONA* mar, int numTimes){
 	if (!mar || numTimes < 1) return false;
@@ -155,9 +180,9 @@ bool inicializarMaratona(MARATONA* mar, int numTimes){
 	return true;
 }
 
-
 /*
-Funcao que recebe o endereco de uma MARATONA e exibe informacoes basicas dos times de acordo com a ordem de classificacao (que e a ordem em que os times estao na lista ligada).
+Funcao que recebe o endereco de uma MARATONA e exibe informacoes basicas dos times de acordo com a ordem de classificacao
+(que e a ordem em que os times estao na lista ligada).
 */
 void mostrarClassificacao(MARATONA* mar){
 	if (mar){
@@ -175,7 +200,8 @@ void mostrarClassificacao(MARATONA* mar){
 }
 
 /*
-Funcao que recebe o endereco de um TIME e exibe informacoes basicas do time (identificador, numero de problemas resolvidos e penalidade total).
+Funcao que recebe o endereco de um TIME e exibe informacoes basicas do time
+(identificador, numero de problemas resolvidos e penalidade total).
 */
 void exibirTime(TIME* t){
 	if (t){
@@ -187,7 +213,6 @@ void exibirTime(TIME* t){
 		printf("\n");
 	}
 }
-
 
 /*
 Funcao que recebe o endereco de uma MARATONA e exibe as informacoes de todos os times (utilizando a funcao exibirTime)
@@ -202,7 +227,6 @@ void exibirTodosTimes(MARATONA* mar){
 		}
 	}
 }
-
 
 /*
 Funcao main que realiza ALGUNS testes nas funcoes implementadas
@@ -248,4 +272,3 @@ int main(){
 	exibirTime(m1.cabeca->prox->time);
 	return 0;
 }
-
